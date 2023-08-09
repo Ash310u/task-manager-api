46e948fbcbe9b17b85b771295110570e6af321aa
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-09 12:14:28 +0530

    Commited testing : abt jest test cases

---
+++ tests/math.test.js
@@ -0,0 +1,19 @@
+test(`sum`, () => {
+// if there is no error then test is passed
+})
+
+test(`sum`, () => {
+    throw new Error('Failure')
+    // and if there is error then test case is fail
+}) 
+
+// 
+// why tests
+// 
+//  - Save time
+//  - Create reliable software
+//  - Gives flexibility to devlopers
+//      - Refactoring
+//      - Collaborating
+//      - Profiling
+//  - Peace of mind
