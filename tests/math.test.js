153826baea44cc51430a1944c98bbe9766a3b806
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-09 15:48:24 +0530

    Commited testing3 : jest provide us these function called expect() and tobe()

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,9 +2,9 @@ const { calculatedTip } = require("../src/tempMath")
 
 test(`Should calculate total with tip`, () => {
     const total = calculatedTip(10, .2)
-
-    if (total !== 12) {
-        throw new Error('Total tip should be 12. got ' + total)
-    }
+    expect(total).toBe(12)
+    // if (total !== 12) {
+    //     throw new Error('Total tip should be 12. got ' + total)
+    // }
     
 }) 
