21357af4e80f9b9af8933fb119b68e27b609bd88
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-10 15:52:01 +0530

    Commited testing6 : Async code using promise 'then, catch' & using async function alternative

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculatedTip, fahrenheitToCelsius, celsiusToFahrenheit } = require("../src/tempMath")
+const { calculatedTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require("../src/tempMath")
 
 test(`Should calculate total with tip`, () => {
     const total = calculatedTip(10, .2)
@@ -20,9 +20,22 @@ test(`C => F`, () => {
     expect(Far).toBe(32)
 })
 
-test(`Async test demo`, (done) => {
-    setTimeout(() => {
-        expect(1).toBe(2) // Assertion
+// test(`Async test demo`, (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2) // Assertion
+//         done()
+//     }, 2000);
+// })
+
+
+test(`Should add two numbers`, (done) => {
+    add(1, 2).then((result) => {
+        expect(result).toBe(3)
         done()
-    }, 2000);
+    })
+})
+
+test(`Should add two numbers async/await`, async () => {
+    const result = await add(10, 20)
+    expect(result).toBe(30)
 })
