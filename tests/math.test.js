4c53d0389761ee349d703f9e677056d733d493da
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-10 14:33:46 +0530

    Commited testing5 : For Async code we have to add a single parameter on to callback function and call it, By doing this jest waits for setTimeout to finish and for our assertion to be created and made before figuring out if the test succeeded or failed

--- tests/math.test.js
+++ tests/math.test.js
@@ -19,3 +19,10 @@ test(`C => F`, () => {
     const Far = celsiusToFahrenheit(0)
     expect(Far).toBe(32)
 })
+
+test(`Async test demo`, (done) => {
+    setTimeout(() => {
+        expect(1).toBe(2) // Assertion
+        done()
+    }, 2000);
+})
