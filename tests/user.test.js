558cd8b638e76c454ab680f83038191c8969a0ac
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-11 13:39:35 +0530

    Commited beforeEach function to wipe out all test database before a test

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,7 +2,12 @@
 // I just want Express application before listen to be called. To make request using supertest.
 
 const request = require('supertest')
-const app = require("../src/app")
+const app = require("../src/app");
+const User = require('../src/models/user');
+
+beforeEach(async ()=> {
+    await User.deleteMany()
+})
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
@@ -10,4 +15,4 @@ test('Should signup a new user', async () => {
         email:'Rick233Sanchez@gmail.com',
         password:'RickSanchez137!'
     }).expect(201)
-})
+}) 
