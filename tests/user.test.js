be2cda456de488b1bc28b0872a9fd71d86f35fe9
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-12 16:29:26 +0530

    Commited test cases for read progile using .set method for authorization

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,34 +2,61 @@
 // I just want Express application before listen to be called. To make request using supertest.
 
 const request = require('supertest')
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const app = require("../src/app");
 const User = require('../src/models/user');
+const auth = require('../src/middleware/auth');
 
-const userone = {
+const userOneId = new mongoose.Types.ObjectId()
+
+const userOne = {
+    _id: userOneId,
     name: 'Dumble Dore',
-    email:'ping.doubledore@hogwarts.com',
-    password:'open.Doubledoor!'
+    email: 'ping.doubledore@hogwarts.com',
+    password: 'open.Doubledoor!',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
 }
 
-beforeEach(async ()=> {
+beforeEach(async () => {
     await User.deleteMany()
-    await new User(userone).save()
+    await new User(userOne).save()
 })
 
 test('Should signup a new user', async () => {
-    await request(app).post('/users').send({
-        name:'Rick Sanchez',
-        email:'Rick233Sanchez@gmail.com',
-        password:'RickSanchez137!'
-    }).expect(201)
-}) 
+    await request(app).post('/users')
+        .send({
+            name: 'Rick Sanchez',
+            email: 'Rick233Sanchez@gmail.com',
+            password: 'RickSanchez137!'
+        })
+        .expect(201)
+})
 
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send(userone).expect(200)
+    await request(app).post('/users/login')
+        .send({
+            email: userOne.email,
+            password: userOne.password
+        })
+        .expect(200)
 })
+
 test('Should not login nonexisting user', async () => {
-    await request(app).post('/users/login').send({
-        email:'nonexisting@exaple.com',
-        password:'testingpass!'
-    }).expect(400)
+    await request(app).post('/users/login')
+        .send({
+            email: 'nonexisting@exaple.com',
+            password: 'testingpass!'
+        })
+        .expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app).get('/users/me')
+        // using ".set" method for setting header we are using "Authorization"
+        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
 })
