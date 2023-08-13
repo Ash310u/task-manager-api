54eafa181f04f9232f8291dc59e4094d0da99949
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-14 00:36:57 +0530

    Commited some new tasks on database for test cases & create a test case fot fetching all tasks for user

--- tests/task.test.js
+++ tests/task.test.js
@@ -4,7 +4,7 @@
 const request = require('supertest')
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
+const { userOneId, userOne, userTwo, setupDatabase } = require('./fixtures/db');
 
 beforeEach(setupDatabase)
 
@@ -20,4 +20,14 @@ test('Should create task for user', async () => {
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
+})
+
+test('Should fetch user tasks', async () => {
+    const response = await request(app).get('/tasks')
+        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    
+    const tasks = response.body
+    expect(tasks.length).toEqual(2)
 })
