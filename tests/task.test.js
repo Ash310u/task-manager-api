ae36ac8bf92fa73953f4e47375b9eaba17152499
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-14 00:59:24 +0530

    Commited new test case called Should not delete other users tasks

--- tests/task.test.js
+++ tests/task.test.js
@@ -4,7 +4,7 @@
 const request = require('supertest')
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const { userOneId, userOne, userTwo, setupDatabase } = require('./fixtures/db');
+const { userOneId, userOne, userTwoId, userTwo, taskOne, taskTwo, taskThree, setupDatabase } = require('./fixtures/db');
 
 beforeEach(setupDatabase)
 
@@ -29,4 +29,14 @@ test('Should fetch user tasks', async () => {
         .expect(200)
 
     expect(response.body.length).toEqual(2)
+})
+
+test('Should not delete other users tasks', async () => {
+    const response = await request(app).delete(`/tasks/${taskOne._id}`)
+        .set(`Authorization`, `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
