60737ac1a0910cab4a06821ab7a14319394c1b5d
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-13 16:18:12 +0530

    Commited new option on to jest command '--runInBand', this is going run tests in series making sure there's no overlap or chance of conflict

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,12 @@
+//  GET access to our Express application but I don't want listen to be called thats why we create "app.js"
+// I just want Express application before listen to be called. To make request using supertest.
+
 const request = require('supertest')
 const Task = require('../src/models/task');
+const app = require('../src/app');
 const { setupDatabase, userOne, userOneId } = require('./fixtures/db');
 
+beforeEach(setupDatabase)
 
 test('Should create task for user', async () => {
 
