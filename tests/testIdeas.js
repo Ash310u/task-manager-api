3379e28b31aa7ef15406c938865cdad46f77dbb9
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-15 16:13:54 +0530

    Commited 'Should not fetch other users task by id ' test case

--- tests/testIdeas.js
+++ tests/testIdeas.js
@@ -4,10 +4,6 @@
 // Task Test Ideas
 
 
-
-// Should fetch user task by id
-// Should not fetch user task by id if unauthenticated
-// Should not fetch other users task by id
 // Should fetch only completed tasks
 // Should fetch only incomplete tasks
 // Should sort tasks by description/completed/createdAt/updatedAt
